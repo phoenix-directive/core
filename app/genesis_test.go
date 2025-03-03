cae3f50654b9ded430ec884878dc6be128483dd0
Core Dev <core@phoenix-directive>
2025-03-03 17:13:54 +0800

    fix: simulation tests

--- app/genesis_test.go
+++ app/genesis_test.go
@@ -254,7 +254,7 @@ func (s *AppGenesisTestSuite) TestMigration() {
 		"params":                 1,
 		"slashing":               3,
 		"staking":                4,
-		"tokenfactory":           3,
+		"tokenfactory":           4,
 		"transfer":               3,
 		"upgrade":                2,
 		"vesting":                1,
@@ -694,7 +694,8 @@ func (s *AppGenesisTestSuite) TestGenesis() {
 				],
 				"denom_creation_gas_consume": "1000000"
 			},
-			"factory_denoms": []
+			"factory_denoms": [],
+			"whitelisted_hooks": []
 		},
 		"transfer": {
 			"port_id": "transfer",
