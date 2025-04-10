ecd7a7ba7c4f00f74d098751877107edcc7ff743
Core Dev <core@phoenix-directive>
2025-04-10 14:44:39 +0800

    fix: update chain upgrade script

--- integration-tests/src/setup/chain-upgrade/upgrade-simulation-17.sh
+++ integration-tests/src/setup/chain-upgrade/upgrade-simulation-17.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OLD_VERSION=release/v2.16
-UPGRADE_HEIGHT=35
+UPGRADE_HEIGHT=30
 CHAIN_ID=test-1
 CHAIN_HOME=$(pwd)/chain-upgrade-data
 CONTRACT_PATH=$(pwd)/src/contracts/counter.wasm
@@ -12,6 +12,8 @@ GOV_PERIOD="3s"
 VAL_MNEMONIC_1="clock post desk civil pottery foster expand merit dash seminar song memory figure uniform spice circle try happy obvious trash crime hybrid hood cushion"
 VAL_MNEMONIC_2="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
 WALLET_MNEMONIC_1="banner spread envelope side kite person disagree path silver will brother under couch edit food venture squirrel civil budget number acquire point work mass"
+WALLET_MNEMONIC_2="veteran try aware erosion drink dance decade comic dawn museum release episode original list ability owner size tuition surface ceiling depth seminar capable only"
+WALLET_MNEMONIC_3="vacuum burst ordinary enact leaf rabbit gather lend left chase park action dish danger green jeans lucky dish mesh language collect acquire waste load"
 
 export OLD_BINARY=$CHAIN_HOME/terrad_old
 export NEW_BINARY=$CHAIN_HOME/terrad_new
@@ -51,6 +53,12 @@ VAL_ADDR_1=$($OLD_BINARY keys show val1 --home $CHAIN_HOME --keyring-backend=tes
 echo $WALLET_MNEMONIC_1 | $OLD_BINARY keys add wallet1 --home $CHAIN_HOME --recover --keyring-backend=test
 WALLET_ADDR_1=$($OLD_BINARY keys show wallet1 --home $CHAIN_HOME --keyring-backend=test --output=json | jq .address -r)
 
+echo $WALLET_MNEMONIC_2 | $OLD_BINARY keys add wallet2 --home $CHAIN_HOME --recover --keyring-backend=test
+WALLET_ADDR_2=$($OLD_BINARY keys show wallet2 --home $CHAIN_HOME --keyring-backend=test --output=json | jq .address -r)
+
+echo $WALLET_MNEMONIC_3 | $OLD_BINARY keys add wallet3 --home $CHAIN_HOME --recover --keyring-backend=test
+WALLET_ADDR_3=$($OLD_BINARY keys show wallet3 --home $CHAIN_HOME --keyring-backend=test --output=json | jq .address -r)
+
 $OLD_BINARY genesis add-genesis-account $($OLD_BINARY --home $CHAIN_HOME keys show val1 --keyring-backend test -a) 100000000000uluna --home $CHAIN_HOME
 
 CURRENT_TIME=$(date +%s)
@@ -81,34 +89,24 @@ sleep 5
 
 VALOPER_ADDR_1=$($OLD_BINARY q staking validators --output=json --home $CHAIN_HOME | jq .validators[0].operator_address -r)
 
-# Create a new token
-echo "Create a new token"
-NO_ECHO=$($OLD_BINARY tx tokenfactory create-denom test --from wallet1 --keyring-backend test --gas auto --gas-adjustment 1.5 --home $CHAIN_HOME --chain-id $CHAIN_ID -y)
+echo "Create new periodic vesting account"
+echo '{
+ 	"start_time": '$(date +%s)',
+ 	"periods":[
+ 		{
+ 			"coins": "10000000uluna",
+ 			"length_seconds": 10000
+ 		},
+ 		{
+ 			"coins": "10000000uluna",
+ 			"length_seconds": 10000
+ 		}
+ 	]
+}' > $CHAIN_HOME/create-periodic-vesting-account.json
+$OLD_BINARY tx vesting create-periodic-vesting-account $WALLET_ADDR_2 $CHAIN_HOME/create-periodic-vesting-account.json --from val1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME -y -o json
 sleep 1
-TOKEN_DENOM=$($OLD_BINARY query tokenfactory denoms-from-creator $WALLET_ADDR_1 --home $CHAIN_HOME --output=json | jq .denoms[0] -r)
-echo "TOKEN_DENOM $TOKEN_DENOM"
-echo "Mint token"
-NO_ECHO=$($OLD_BINARY tx tokenfactory mint 1000000000$TOKEN_DENOM --from wallet1 --keyring-backend test --gas auto --gas-adjustment 1.5 --home $CHAIN_HOME --chain-id $CHAIN_ID -y)
+$OLD_BINARY tx vesting create-periodic-vesting-account $WALLET_ADDR_3 $CHAIN_HOME/create-periodic-vesting-account.json --from val1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME -y -o json
 sleep 1
-
-# Upload a contract
-echo "Upload a contract"
-# NO_ECHO=$($OLD_BINARY tx wasm store $CONTRACT_PATH --from wallet1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME -y)
-NO_ECHO=$($OLD_BINARY tx wasm store $CONTRACT_PATH --from wallet1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME --gas auto --gas-adjustment 1.5 -y)
-sleep 1
-
-# Instantiate a contract
-echo "Instantiate a contract"
-NO_ECHO=$($OLD_BINARY tx wasm instantiate 1 '{"count":0}' --amount 100000000$TOKEN_DENOM --from wallet1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME --label "counter" --no-admin -y)
-sleep 1
-CONTRACT_ADDRESS=$($OLD_BINARY query wasm list-contract-by-code 1 --output=json --home $CHAIN_HOME | jq .contracts[0] -r)
-echo "CONTRACT_ADDRESS $CONTRACT_ADDRESS"
-CONTRACT_BALANCE=$($OLD_BINARY query bank balances $CONTRACT_ADDRESS --home $CHAIN_HOME --output=json | jq ".balances[0].amount")
-if [[ "$CONTRACT_BALANCE" == "100000000" ]]; then
-    echo "Contract balance is less than 100000000"
-    exit 1
-fi
-
 
 GOV_ADDRESS=$($OLD_BINARY query auth module-account gov --home $CHAIN_HOME --output json | jq .account.base_account.address -r)
 echo '{
@@ -155,14 +153,16 @@ sleep 1
 # run new binary
 echo "Starting new binary"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    screen -L -Logfile $CHAIN_HOME/log-screen.log -dmS node1 $NEW_BINARY start --log_level trace --log_format json --home $CHAIN_HOME --pruning=nothing
+    screen -L -dmS node1 $NEW_BINARY start --log_level trace --log_format json --home $CHAIN_HOME --pruning=nothing
 else
     screen -L -Logfile $CHAIN_HOME/log-screen.log -dmS node1 $NEW_BINARY start --log_level trace --log_format json --home $CHAIN_HOME --pruning=nothing
 fi
 sleep 15
 
+echo "Upgrade successful"
+
 # get new block height
-NEW_BLOCK_HEIGHT=$($NEW_BINARY status --home $CHAIN_HOME | jq '.SyncInfo.latest_block_height' -r)
+NEW_BLOCK_HEIGHT=$($NEW_BINARY status --home $CHAIN_HOME | jq '.sync_info.latest_block_height' -r)
 echo "NEW_BLOCK_HEIGHT $NEW_BLOCK_HEIGHT"
 
 # assert that the new block height is greater than the upgrade height
@@ -171,6 +171,36 @@ if [ $NEW_BLOCK_HEIGHT -le $UPGRADE_HEIGHT ]; then
     exit 1
 fi
 
-echo "Upgrade successful"
+
+echo "Performing some sanity checks"
+
+# Create a new token
+echo "Create a new token"
+NO_ECHO=$($NEW_BINARY tx tokenfactory create-denom test --from wallet1 --keyring-backend test --gas auto --gas-adjustment 1.5 --home $CHAIN_HOME --chain-id $CHAIN_ID -y)
+sleep 1
+TOKEN_DENOM=$($NEW_BINARY query tokenfactory denoms-from-creator $WALLET_ADDR_1 --home $CHAIN_HOME --output=json | jq .denoms[0] -r)
+echo "TOKEN_DENOM $TOKEN_DENOM"
+echo "Mint token"
+NO_ECHO=$($NEW_BINARY tx tokenfactory mint 1000000000$TOKEN_DENOM --from wallet1 --keyring-backend test --gas auto --gas-adjustment 1.5 --home $CHAIN_HOME --chain-id $CHAIN_ID -y)
+sleep 1
+
+# Upload a contract
+echo "Upload a contract"
+# NO_ECHO=$($OLD_BINARY tx wasm store $CONTRACT_PATH --from wallet1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME -y)
+NO_ECHO=$($NEW_BINARY tx wasm store $CONTRACT_PATH --from wallet1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME --gas auto --gas-adjustment 1.5 -y)
+sleep 1
+
+# Instantiate a contract
+echo "Instantiate a contract"
+NO_ECHO=$($NEW_BINARY tx wasm instantiate 1 '{"count":0}' --amount 100000000$TOKEN_DENOM --from wallet1 --keyring-backend test --chain-id $CHAIN_ID --home $CHAIN_HOME --label "counter" --no-admin -y)
+sleep 1
+CONTRACT_ADDRESS=$($NEW_BINARY query wasm list-contract-by-code 1 --output=json --home $CHAIN_HOME | jq .contracts[0] -r)
+echo "CONTRACT_ADDRESS $CONTRACT_ADDRESS"
+CONTRACT_BALANCE=$($NEW_BINARY query bank balances $CONTRACT_ADDRESS --home $CHAIN_HOME --output=json | jq ".balances[0].amount")
+if [[ "$CONTRACT_BALANCE" == "100000000" ]]; then
+    echo "Contract balance is less than 100000000"
+    exit 1
+fi
+
 # kill screen
 screen -X -S node1 quit
